3875da0e3f67d3c13e14cec68fad159a7eb4ee50
claud <claud@Claudia>
2024-11-22 15:41:51 +0100

    Claudiaaa

--- src/main/java/com/mycompany/testinginf/mainClass.java
+++ src/main/java/com/mycompany/testinginf/mainClass.java
@@ -12,5 +12,6 @@ package com.mycompany.testinginf;
 public class mainClass {
     public static void main(String[] args){
         System.out.println("Ciao ");
+        System.out.println("Claudiaaaa");
     }
 }
